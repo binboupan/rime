da82e09c03dcbad9f14908c029b6554fa6fe3ad4
Ville Salminen <nullcollision@kagura.eu>
2024-10-30 20:05:51 +0200

    Add adguard plugin

--- src/plugins/adguardhome.py
+++ src/plugins/adguardhome.py
@@ -1,29 +1,32 @@
-# Gets blocked count for Pihole
+# Adguard Home Plugin
 
 
 from fastapi import FastAPI, APIRouter, Body, status
 import requests
 from fastapi.responses import JSONResponse
 from dependencies import Plugin
+from requests.auth import HTTPBasicAuth
 
 from yamlutil.database import get_config
+import base64
 
 #from database.database import get_plugin_config
 
-class WebsitePlugin(Plugin):
+class AdguardPlugin(Plugin):
     def register(self, app: FastAPI):
-        print("Load plugin: Pihole stats")
+        print("Load plugin: Adguard stats")
         router = APIRouter()
 
         # Add the routes your plugin uses here
-        @router.get("/pihole/status")
+        @router.get("/adguardhome/status")
         def get_status(id: str, category: str):
             config = get_config()
 
-            print(id, category)
+            authentication = base64.b64decode(config[category][id]["api-key"]).decode("utf-8").split(":")
 
+            #HTTPBasicAuth('user', 'pass')
             try:
-                response = requests.get(config[category][id]["url"] + "/admin/api.php?summaryRaw&auth=" + config[category][id]["api-key"])
+                response = requests.get(config[category][id]["url"] + "/control/stats", auth=HTTPBasicAuth(authentication[0], authentication[1]))
                 
                 if response.status_code == 200 or response.status_code == 302 or response.status_code == 304:
                     return JSONResponse(content=response.json())
